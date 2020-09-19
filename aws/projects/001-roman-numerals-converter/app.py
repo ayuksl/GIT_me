0603c068179226ad2d3c5a4f0c9cd7bad7957012
ayuksl <yuksel93@yahoo.com>
2020-09-19 22:33:02 +0200

    roman_nums

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -7,11 +7,11 @@ def index():
     if request.method == 'POST':    
         user_input = request.form["number"]
         if user_input.isalpha() or user_input == "0":
-            return render_template("index.html", developer_name = "E2227", not_valid= True)
+            return render_template("index.html", developer_name = "E2438", not_valid= True)
         else:
             user_input = int(user_input)
         if user_input >= 4000 or user_input < 1 :
-            return render_template("index.html", developer_name = "E2227", not_valid= True)
+            return render_template("index.html", developer_name = "E2438", not_valid= True)
         else:
             val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
             syb = ["M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV", "I"]
@@ -23,9 +23,9 @@ def index():
 	                number_roman += syb[i]
 	                user_input -= val[i]
 	            i += 1
-            return render_template("result.html", developer_name = "E2227", number_decimal = number_decimal, number_roman=number_roman)
+            return render_template("result.html", developer_name = "E2438", number_decimal = number_decimal, number_roman=number_roman)
     else:
-        return render_template("index.html", developer_name = "E2227")
+        return render_template("index.html", developer_name = "E2438")
         
 
 if __name__ == '__main__':
